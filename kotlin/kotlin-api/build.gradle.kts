4751e528bd8ec184117db1e2280f1b15479c6722
kimdongho <cha11enge@hanmail.net>
2025-12-13 19:26:39 +0900

    [DOCKER] 도커 설정 완료

--- kotlin/kotlin-api/build.gradle.kts
+++ kotlin/kotlin-api/build.gradle.kts
@@ -3,8 +3,8 @@ val springBootVersion: String by rootProject.extra
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot")
-    id("io.spring.dependency-management")
+    id("org.springframework.boot") version "3.5.8"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.spring"
