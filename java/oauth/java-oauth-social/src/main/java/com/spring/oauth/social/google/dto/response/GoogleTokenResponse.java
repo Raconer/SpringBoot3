7f3c09a91774b38e0dfeaa1fd9593978129e83a1
Dongho Kim <cha11enge@hanmail.net>
2026-01-02 18:44:38 +0900

    [OAUTH] Google Oauth 추가

---
+++ java/oauth/java-oauth-social/src/main/java/com/spring/oauth/social/google/dto/response/GoogleTokenResponse.java
@@ -0,0 +1,25 @@
+package com.spring.oauth.social.google.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class GoogleTokenResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("id_token")
+    private String idToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("expires_in")
+    private Integer expiresIn;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;  // 첫 인증 시에만 옴 (access_type=offline일 때)
+
+    @JsonProperty("scope")
+    private String scope;
+}
